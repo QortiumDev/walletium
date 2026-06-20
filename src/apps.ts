19ed6ad5ad54580b83cf49e3a76a7b9c5ae261df
7r15 <287508559+7r15@users.noreply.github.com>
2026-06-19 22:51:06 -0400

    app: add apps cross-link entry

--- src/apps.ts
+++ src/apps.ts
@@ -1,4 +1,5 @@
 export const APPS = {
+  apps:   { qdn: 'Apps',   label: 'Apps'   },
   chain: { qdn: 'Chain', label: 'Chain' },
   groups: { qdn: 'Groups', label: 'Groups' },
   names: { qdn: 'Names', label: 'Names' },
